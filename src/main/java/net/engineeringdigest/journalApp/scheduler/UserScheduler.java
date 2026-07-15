f9961f53fbe06e2552059e578cf04c0f15d74c5b
mayank01 <133534524+mayankkk04@users.noreply.github.com>
2026-07-16 00:17:11 +0530

    Adding Sentiment analysis.

--- src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
+++ src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -4,6 +4,7 @@ import lombok.extern.apachecommons.CommonsLog;
 import net.engineeringdigest.journalApp.cache.AppCache;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.enums.Sentiment;
 import net.engineeringdigest.journalApp.repository.UserRepositoryImpl;
 import net.engineeringdigest.journalApp.service.EmailService;
 import net.engineeringdigest.journalApp.service.SentimentAnalysisService;
@@ -13,7 +14,9 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
@@ -30,16 +33,28 @@ public class UserScheduler {
     @Autowired
     private AppCache appCache;
 
-    @Scheduled(cron = "0 9 * * SUN") //scheduler for when to run this method automatically
+//    @Scheduled(cron = "0 9 * * SUN") //scheduler for when to run this method automatically
     public void fetchUserAndSendSaMail(){
         List<User> users = userRepository.getUsersForSA();
         for(User user: users){
             List<JournalEntry> journalEntries = user.getJournalEntries();
             //get last 7 days journal entries
-            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
-            String entry = String.join(" ", filteredEntries);
-            String sentiment = sentimentAnalysisService.getSentiment(entry);
-            emailService.sendEmail(user.getEmail(), "Sentiment for last  days.", sentiment);
+            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
+            Map<Sentiment, Integer> sentimentCounts = new HashMap<>();
+            for(Sentiment sentiment : sentiments)
+                if(sentiment != null)
+                    sentimentCounts.put(sentiment, sentimentCounts.getOrDefault(sentiment,0)+1);
+            Sentiment mostFrequentSentiment = null;
+            int maxCount = 0;
+            for(Map.Entry<Sentiment, Integer> entry : sentimentCounts.entrySet()){
+                if(entry.getValue() > maxCount){
+                    maxCount = entry.getValue();
+                    mostFrequentSentiment = entry.getKey();
+                }
+            }
+            if(mostFrequentSentiment != null){
+                emailService.sendEmail(user.getEmail(), "Sentiment for last  days.", sentiment);
+            }
         }
 
     }
